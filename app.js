13f0a1d7fec819d61cdf657c214c65aac7764275
tejasvijb <jbtejasvi@gmail.com>
2024-04-22 22:00:48 +0530

    install dependencies

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(404).json({message: 'Hello World!', app: 'Natours'})
+})
+
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint...')
+})
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
